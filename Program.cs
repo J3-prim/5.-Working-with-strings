ee0e05ab24c2bfed58e2adedab28993b263864eb
Justin Prim <justinprim140969@gmail.com>
2024-01-10 14:28:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Working_with_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write line");
+            Console.WriteLine("Write \n line");
+            Console.WriteLine("Write \" line");
+
+            string firstname = "Justin";
+            string lastname = "Prim";
+
+            Console.WriteLine(lastname + " "+firstname);
+
+            Console.WriteLine(lastname.Length);
+            Console.WriteLine(firstname.ToUpper());
+            Console.WriteLine(lastname.Contains("Pr"));
+            Console.WriteLine(firstname[2]);
+            Console.WriteLine(lastname.IndexOf("i"));
+            Console.WriteLine(firstname.Substring(1));
+            Console.WriteLine(lastname.Substring(1,2));
+
+            Console.ReadLine();
+        }
+    }
+}
